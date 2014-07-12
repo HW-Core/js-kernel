e9494e18e2512c33e60d03c8a71ab697969dad4c
Yehonal <hw.2@hotmail.it>
2014-07-12 17:33:05 +0200

    first commit

---
+++ Bootstrap.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2007 - 2014 Hyperweb2 All rights reserved.
+ * GNU General Public License version 3; see www.hyperweb2.com/terms/
+ */
+
+var Hw2 = Hw2 || {}; // namespace
+
+var HW2PATH_SHARE = HW2PATH_ROOT + "share/";
+var HW2PATH_LOCAL = HW2PATH_ROOT + "local/";
+var HW2PATH_JS_LIB = HW2PATH_SHARE + "modules/Hw2/Js/Library/";
+
+document.write("<script src='" + HW2PATH_SHARE + "modules/jquery/jquery/dist/jquery.js'><\/script>");
+
+window.onload = function() {
+    // create custom alias for jquery to avoid conflicts
+    Hw2.JQ = jQuery.noConflict(true);
+
+    Hw2.JQ.ajaxSetup({async: false});
+    Hw2.JQ.getScript(HW2PATH_JS_LIB + "Application.js");
+    Hw2.JQ.ajaxSetup({async: true});
+
+    var App = Hw2.Application;
+
+    App.loadResource(HW2PATH_JS_LIB + "JsMethods.js", "js");
+    App.loadResource(HW2PATH_JS_LIB + "Namespacer.js", "js");
+    //run local bootstrap
+    App.loadResource(HW2PATH_LOCAL + "init/Bootstrap.js", "js", true);
+};
+
+//# sourceURL=share/bootstrap.js
