3ed0b428e244b24aea046676b47b8ddb7adbbcfd
Yehonal <hw.2@hotmail.it>
2014-07-14 19:13:48 +0200

    added hook script and new dependencies

---
+++ _hw2/installer_hook.js
@@ -0,0 +1,9 @@
+module.exports=function(event,basePath,modulePath) {
+    
+    if (event==="postinstall") {
+        var fs = require('fs');
+
+        fs.createReadStream(modulePath+'_hw2/index_files/index.html')
+                .pipe(fs.createWriteStream(basePath+'/../index.html')); // copy to root
+    }
+};
