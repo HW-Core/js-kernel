7565d62bcdda4c25d47dc197104e3a19c512d6fb
Yehonal <hw.2@hotmail.it>
2014-07-15 23:28:13 +0200

    added keep section to json

--- _hw2/installer_hook.js
+++ _hw2/installer_hook.js
@@ -2,8 +2,8 @@ module.exports=function(event,basePath,modulePath) {
     
     if (event=="postinstall") {
         var fs = require('fs');
-
+        // copy to root overwriting existing if any
         fs.createReadStream(modulePath+'/_hw2/index_files/index.html')
-                .pipe(fs.createWriteStream(basePath+'/index.html')); // copy to root
+                .pipe(fs.createWriteStream(basePath+'/index.html')); 
     }
 };
