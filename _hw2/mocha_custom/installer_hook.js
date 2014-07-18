16b2563a82c18a688e7c41ce9fb771cc0f87d43d
Yehonal <hw.2@hotmail.it>
2014-07-18 23:14:27 +0200

    Just a message

--- _hw2/mocha_custom/installer_hook.js
+++ _hw2/mocha_custom/installer_hook.js
@@ -3,6 +3,7 @@ var exec = require('child_process').exec;
 module.exports = function(action, config, modulePath) {
 
     if (action == "postinstall") {
+        console.log('Installing mocha node modules...');
         exec('npm install', {"cwd": modulePath}, function(error, stdout, stderr) {
             if (stderr !== null) {
                 console.log('' + stderr);
