1da77a00affed6a29eef51468a004e9766cd0426
Yehonal <hw.2@hotmail.it>
2014-07-18 19:13:53 +0200

    use mocha as test framework instead qunit

--- _hw2/installer_hook.js
+++ _hw2/installer_hook.js
@@ -1,9 +1,18 @@
-module.exports=function(event,basePath,modulePath) {
+module.exports=function(action,basePath,modulePath) {
     
-    if (event=="postinstall") {
+    if (action=="postinstall") {
         var fs = require('fs');
         // copy to root overwriting existing if any
         fs.createReadStream(modulePath+'/_hw2/index_files/index.html')
                 .pipe(fs.createWriteStream(basePath+'/index.html')); 
+
+        var mocha=basePath+'/modules/core-dep/mocha/';
+        fs.createReadStream(modulePath+'/_hw2/mocha_custom/bower.custom.json')
+                .pipe(fs.createWriteStream(mocha+'bower.custom.json')); 
+
+        fs.mkdir(mocha+"_hw2",function(e){
+            fs.createReadStream(modulePath+'/_hw2/mocha_custom/installer_hook.json')
+                    .pipe(fs.createWriteStream(mocha+'_hw2/installer_hook.json)); 
+        }
     }
 };
