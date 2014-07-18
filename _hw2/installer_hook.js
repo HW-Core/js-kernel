25ea1c75bcede34c403be4531e539cd90decf75e
Yehonal <hw.2@hotmail.it>
2014-07-19 00:20:30 +0200

    fix another bug with paths

--- _hw2/installer_hook.js
+++ _hw2/installer_hook.js
@@ -1,18 +1,18 @@
 var path=require("path");
 
 module.exports = function(action, config, modulePath) {
+    config = config || {};
 
     if (action == "postinstall") {
         var fs = require('fs');
         config.cwd=config.cwd || process.cwd();
         config.directory=config.directory || "";
 
-        var modulePath=path.join(config.cwd,config.directory);
         // copy to root overwriting existing if any
         fs.createReadStream(modulePath + '/_hw2/index_files/index.html')
                 .pipe(fs.createWriteStream( path.join(config.cwd , 'index.html') ));
 
-        var mocha = path.join(modulePath, 'core-dep/mocha/');
+        var mocha = path.join(config.cwd,config.directory, 'core-dep/mocha/');
         fs.createReadStream(path.join(modulePath,'_hw2/mocha_custom/bower.custom.json'))
                 .pipe(fs.createWriteStream(mocha + 'bower.custom.json'));
 
