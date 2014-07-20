6b4bb4c5f683494cc7d33e397ef0722e016aa741
Yehonal <hw.2@hotmail.it>
2014-07-20 02:51:40 +0200

    added exec logs for better debug

--- _hw2/installer_hook.js
+++ _hw2/installer_hook.js
@@ -1,27 +1,39 @@
-var path=require("path");
+var path = require("path");
 
 module.exports = function(action, config, modulePath) {
     if (action == "postinstall") {
         console.log("Kernel postinstall event...");
 
         var fs = require('fs');
-        config.cwd=config.cwd || process.cwd();
+        config.cwd = config.cwd || process.cwd();
 
         // copy to root overwriting existing if any
         fs.createReadStream(modulePath + '/_hw2/index_files/index.html')
-                .pipe(fs.createWriteStream( path.join(config.cwd , 'index.html') ));
+                .pipe(fs.createWriteStream(path.join(config.cwd, 'index.html')));
 
-        var mocha = path.join(config.cwd,config.directory, 'core-dep/mocha/');
-        fs.createReadStream(path.join(modulePath,'_hw2/mocha_custom/bower.custom.json'))
+        var mocha = path.join(config.cwd, config.directory, 'core-dep/mocha/');
+        fs.createReadStream(path.join(modulePath, '_hw2/mocha_custom/bower.custom.json'))
                 .pipe(fs.createWriteStream(mocha + 'bower.custom.json'));
 
-        fs.mkdir(path.join(mocha , "_hw2"), function(e) {
+        fs.mkdir(path.join(mocha, "_hw2"), function(e) {
             fs.createReadStream(path.join(modulePath, '/_hw2/mocha_custom/installer_hook.js'))
-                    .pipe(fs.createWriteStream(path.join(mocha , '_hw2/installer_hook.js')));
+                    .pipe(fs.createWriteStream(path.join(mocha, '_hw2/installer_hook.js')));
 
             var exec = require('child_process').exec;
 
-            exec("hw2-bower cache clean mocha && hw2-bower update core-dep/mocha --force", {cwd:config.cwd});
+            exec("hw2-bower cache clean mocha && hw2-bower update core-dep/mocha --force", {cwd: config.cwd}, function(error, stdout, stderr) {
+                console.log('Installing mocha node modules...');
+
+                if (stderr !== null) {
+                    console.log('' + stderr);
+                }
+                if (stdout !== null) {
+                    console.log('' + stdout);
+                }
+                if (error !== null) {
+                    console.log('' + error);
+                }
+            });
         });
     }
 };
