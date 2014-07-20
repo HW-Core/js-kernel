19cb9c11703f623cdebc9bd52c4ba766ba5b11e8
Yehonal <hw.2@hotmail.it>
2014-07-20 02:40:40 +0200

    "case" mistake

--- _hw2/installer_hook.js
+++ _hw2/installer_hook.js
@@ -2,7 +2,7 @@ var path=require("path");
 
 module.exports = function(action, config, modulePath) {
     if (action == "postinstall") {
-        Console.log("Kernel postinstall event...");
+        console.log("Kernel postinstall event...");
 
         var fs = require('fs');
         config.cwd=config.cwd || process.cwd();
