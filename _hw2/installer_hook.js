0e2c0d639431c504519ee6fbea61ff9f52e5ec85
Yehonal <hw.2@hotmail.it>
2014-07-30 17:52:23 +0200

    fixed hooks

--- _hw2/installer_hook.js
+++ _hw2/installer_hook.js
@@ -1,6 +1,6 @@
 var path = require("path");
 
-module.exports = function(action, config, modulePath) {
+module.exports = function(action,config,name,pkgPath,newMeta,oldMeta,callback) {
     if (action == "postinstall") {
         console.log("Kernel postinstall event...");
 
@@ -8,21 +8,21 @@ module.exports = function(action, config, modulePath) {
         config.cwd = config.cwd || process.cwd();
 
         // copy to root overwriting existing if any
-        fs.createReadStream(modulePath + '/_hw2/index_files/index.html')
+        fs.createReadStream(pkgPath + '/_hw2/index_files/index.html')
                 .pipe(fs.createWriteStream(path.join(config.cwd, 'index.html')));
 
-        var mocha = path.join(config.cwd, config.directory, 'core-dep/mocha/');
-        fs.createReadStream(path.join(modulePath, '_hw2/mocha_custom/bower.custom.json'))
+        var mocha = path.join(config.cwd, config.directory, 'Hw2/dep/mocha/');
+        fs.createReadStream(path.join(pkgPath, '_hw2/mocha_custom/bower.custom.json'))
                 .pipe(fs.createWriteStream(mocha + 'bower.custom.json'));
 
         // workaround: to avoid _hw2 folder deletations after update we've
         // to update .bower.json right now since the "keep" procedure
         // take care of previous json, not new one
-        fs.createReadStream(path.join(modulePath, '_hw2/mocha_custom/bower.custom.json'))
+        fs.createReadStream(path.join(pkgPath, '_hw2/mocha_custom/bower.custom.json'))
                 .pipe(fs.createWriteStream(mocha + '.bower.json'));
 
         fs.mkdir(path.join(mocha, "_hw2"), function(e) {
-            fs.createReadStream(path.join(modulePath, '/_hw2/mocha_custom/installer_hook.js'))
+            fs.createReadStream(path.join(pkgPath, '/_hw2/mocha_custom/installer_hook.js'))
                     .pipe(fs.createWriteStream(path.join(mocha, '_hw2/installer_hook.js')));
 
             var exec = require('child_process').exec;
--- _hw2/mocha_custom/installer_hook.js
+++ _hw2/mocha_custom/installer_hook.js
@@ -1,9 +1,9 @@
 var exec = require('child_process').exec;
 
-module.exports = function(action, config, modulePath) {
+module.exports = function(action,config,name,pkgPath,newMeta,oldMeta,callback) {
 
     if (action == "postinstall") {
-        exec('npm install', {"cwd": modulePath}, function(error, stdout, stderr) {
+        exec('npm install', {"cwd": pkgPath}, function(error, stdout, stderr) {
             console.log('Installing mocha node modules...');
 
             if (stderr !== null) {
