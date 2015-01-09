41cdb8ceeb48b0517304cd01d58007a8288f0c18
Yehonal <hw.2@hotmail.it>
2015-01-09 16:19:44 +0100

    Updated

--- js/index.html.js
+++ js/index.html.js
@@ -5,49 +5,9 @@
 
 hw2.exports = function () {
     var $ = this;
-    $.Loader.load([
-        $.const.PATH_JS_LIB + "browser/common/Loader.js",
-        $.const.PATH_JS_LIB + "browser/common/Uri.js",
-        $.const.PATH_JS_LIB + "browser/common/Events.js"
-    ], function () {
-        // NAVIGATOR
-        var justLoading = null;
-
-        function loadPage (page) {
-            if (!justLoading || justLoading !== page) {
-                justLoading = page;
-                $.Browser.Loader.load("pages/" + page + ".html", checkNavigation, {selector: "#dyn-content"});
-            }
-        }
-
-        function checkNavigation () {
-            $.Browser.Events.onBodyLoad(function () {
-                var pages = ["home", "class", "class-friendly", "class-basic", "loader", "installation"];
-
-                pages.forEach(function (page) {
-                    $.Browser.JQ(".nav-" + page).each(function (id, el) {
-                        var element = $.Browser.JQ(el);
-                        var evt = $.Browser.JQ._data(el, 'events');
-                        if (!evt || !evt.mousedown) {
-                            element.mousedown(function (evt) {
-                                if (event.which === 1) // left mouse click
-                                    loadPage(page);
-                            });
-                        }
-                    });
-                });
-            });
-        }
-
-        var getPage = function  () {
-            return new this.Browser.Uri(document.location.href).getFragment() || "home";
-        }.bind($);
-
-        window.addEventListener("popstate", function (e) {
-            loadPage(getPage());
-        });
-
-        loadPage(getPage());
+    $.Loader.load(["hw2!PATH_JS_LIB:browser/router/Navigation.js"],function(Navigation) {
+        var pages = ["home", "class", "class-friendly", "class-basic", "loader", "installation"];
+        var nav = new Navigation(pages);
+        nav.init();
     });
-
 };
