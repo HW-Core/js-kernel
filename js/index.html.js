071cceac962df6f21a454bf7524a916d1bf6ba92
Yehonal <hw.2@hotmail.it>
2015-05-08 15:55:35 +0200

    various fixes and changes

--- js/index.html.js
+++ js/index.html.js
@@ -1,13 +1,52 @@
-/*
- * Copyright (C) 2007 - 2014 Hyperweb2 All rights reserved.
- * GNU General Public License version 3; see www.hyperweb2.com/terms/
- */
-
-hw2.exports = function () {
+hw2.include([
+    "hw2!{PATH_JS_LIB}browser/application/Component.js",
+    "hw2!{PATH_JS_LIB}browser/application/System.js"
+]).define(function () {
     var $ = this;
-    $.Loader.load(["hw2!{PATH_JS_LIB}browser/router/Navigation.js"],function(Navigation) {
-        var pages = ["home", "class", "class-friendly", "class-basic", "loader", "installation"];
-        var nav = new Navigation(pages);
-        nav.init();
-    });
-};
+
+    var Main = $.class.extends($.Browser.Component)([
+        $.public({
+            __construct: function (parent, childs, opt) {
+                var comp = parent.getRouter().getRouteInfo().getComponent();
+                var page = comp || "home";
+
+                var template = new $.Browser.Template("pages/" + page + ".html", "styles/index.css");
+                opt.template = template;
+                this.__super(parent, [], opt);
+            },
+            update: function () {
+                this.__super();
+            },
+            init: function () {
+                var that = this;
+                this.__super().then(function () {
+                    var pages = ["home", "loader", "installation","getting-started","how-to-create-a-module"];
+
+                    for (i in pages) {
+                        that.i.getRouter().setRoute(".nav-" + pages[i], {component: pages[i]});
+                    }
+                });
+
+            },
+            build: function () {
+                this.__super();
+
+                var comp = this.i.getRouter().getRouteInfo().getComponent();
+                var page = comp || "home";
+
+                $.Browser.Loader.load("pages/" + page + ".html", null, {selector: "#dyn-content"});
+            }
+        }),
+        $.private({
+            prevContent: null
+        })
+    ]);
+
+    var system = new $.Browser.System(true);
+
+    system.register("main", Main, {autoStart: true, selector: "#dyn-content"});
+
+    system.init();
+
+});
+;
