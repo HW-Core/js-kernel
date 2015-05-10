595eb98c407a586a881c1a82e967bd2641461173
Yehonal <hw.2@hotmail.it>
2015-05-11 01:12:31 +0200

    fixed typeCompare and added getPropertyDescriptor and getPropertyNames for "Object"

--- utils.js
+++ utils.js
@@ -6,12 +6,40 @@
 'use strict';
 
 hw2.define(function () {
-    var $=this;
-    
+    var $ = this;
+
+    // property getter that walks in chain
+    !function (Object, getPropertyDescriptor, getPropertyNames) {
+        if (!(getPropertyDescriptor in Object)) {
+            var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
+            Object[getPropertyDescriptor] = function getPropertyDescriptor (o, name) {
+                var proto = o, descriptor;
+                while (proto && !(
+                    descriptor = getOwnPropertyDescriptor(proto, name))
+                    )
+                    proto = proto.__proto__;
+                return descriptor;
+            };
+        }
+        if (!(getPropertyNames in Object)) {
+            var getOwnPropertyNames = Object.getOwnPropertyNames, ObjectProto = Object.prototype, keys = Object.keys;
+            Object[getPropertyNames] = function getPropertyNames (o) {
+                var proto = o, unique = {}, names, i;
+                while (proto != ObjectProto) {
+                    for (names = getOwnPropertyNames(proto), i = 0; i < names.length; i++) {
+                        unique[names[i]] = true;
+                    }
+                    proto = proto.__proto__;
+                }
+                return keys(unique);
+            };
+        }
+    }(Object, "getPropertyDescriptor", "getPropertyNames");
+
     $.typeOf = function (obj) {
         return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
     };
-    
+
     /**
      * 
      * @param {type} type
@@ -19,7 +47,10 @@ hw2.define(function () {
      * @param {type} skipError, if set to true then return a boolean instead throw an error
      * @returns {undefined}
      */
-    $.typeCompare = function (type, val,skipError) {
+    $.typeCompare = function (type, val, skipError) {
+        if (val === null || val === undefined)
+            return true;
+
         if (typeof type === "string") {
             var t = $.typeOf(val);
             if (t !== type) {
@@ -36,11 +67,11 @@ hw2.define(function () {
                 throw new TypeError("Incompatible type: " + typeof val + " , excepted " + type.name);
             }
         }
-        
+
         return true;
     };
-    
+
     $.typeFn = function (/* arguments type, function */) {
-        
+
     };
 });
