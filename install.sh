2c07e360d05be112a1cd865a9b043b4196974e76
Yehonal <hw.2@hotmail.it>
2016-07-18 20:18:10 +0200

    fixed mocha path and minor bug in install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ function add_repo() {
     basedir=$4
     path=$DIR/$basedir/$name
     
-    (git --git-dir=$path/.git/ rev-parse && git --git-dir=$path/.git/ pull origin $branch) || git clone $url/$name.git -b $branch $path 
+    ([ -e $path/.git/ ] && git --git-dir=$path/.git/ rev-parse && git --git-dir=$path/.git/ pull origin $branch) || git clone $url/$name.git -b $branch $path 
 	[ -f $path/install.sh ] && bash $path/install.sh $params
 }
 
@@ -32,7 +32,7 @@ add_file "https://raw.githubusercontent.com/components/rsvp.js/d2740bfe52a3a3d63
 add_file "https://raw.githubusercontent.com/Benvie/WeakMap/2693ff73a1650dc7b8beeeb6ce0eba79b53a66af/weakmap.js" $mod_path"/weakmap/index.js"
 
 if [[ $1 == "dev" ]]; then
-    add_repo "https://github.com/visionmedia/" "mocha" "1.20.1" $mod_path
+    add_repo "https://github.com/mochajs/" "mocha" "1.20.1" $mod_path
     
     add_file "http://sinonjs.org/releases/sinon-1.10.3.js" $mod_path"/sinon/index.js"
     add_file "http://chaijs.com/chai.js" $mod_path"/chai/index.js"
