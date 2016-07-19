cde93d65c5aa3d7d3f58f0d2af25e2a214e64f82
Yehonal <hw.2@hotmail.it>
2016-07-19 08:58:32 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PATH_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
-[ ! -d $PATH_ROOT ] && git clone https://github.com/udw/joiner $PATH_ROOT/udw/joiner -b master
-source "$PATH_ROOT/udw/joiner/joiner.sh"
+PATH_MODULES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
+[ ! -d $PATH_MODULES/udw/joiner ] && git clone https://github.com/udw/joiner $PATH_MODULES/udw/joiner -b master
+source "$PATH_MODULES/udw/joiner/joiner.sh"
 
 
 #
