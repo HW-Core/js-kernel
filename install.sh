5f5dc8f0bccef438a8542a9e66aa8103b77eef26
Yehonal <yehonal.azeroth@gmail.com>
2016-10-04 10:39:21 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH_MODULES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
-[ ! -d $PATH_MODULES/udw/joiner ] && git clone https://github.com/udw/joiner $PATH_MODULES/udw/joiner -b master
-source "$PATH_MODULES/udw/joiner/joiner.sh"
+[ ! -d $PATH_MODULES/uwd/joiner ] && git clone https://github.com/uw-dev/joiner $PATH_MODULES/uwd/joiner -b master
+source "$PATH_MODULES/uwd/joiner/joiner.sh"
 
 
 #
