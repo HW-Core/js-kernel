5db71a9a1531ed89f354b4b6992a293ab7b06dc0
Yehonal <yehonal.azeroth@gmail.com>
2016-10-07 16:46:54 +0200

    using "with_dev" function from joiner

--- install.sh
+++ install.sh
@@ -9,12 +9,13 @@ source "$PATH_MODULES/uwd/joiner/joiner.sh"
 #
 
 mod_path="hw-core/js-modules/"
+
 Joiner:add_file "http://requirejs.org/docs/release/2.1.15/r.js" $mod_path"/requirejs/r/index.js"
 Joiner:add_file "http://requirejs.org/docs/release/2.1.15/minified/require.js" $mod_path"/requirejs/requirejs/index.js"
 Joiner:add_file "https://raw.githubusercontent.com/components/rsvp.js/d2740bfe52a3a3d63bb132d45490d3126bf0e065/rsvp.js" $mod_path"/rsvp/index.js"
 Joiner:add_file "https://raw.githubusercontent.com/Benvie/WeakMap/2693ff73a1650dc7b8beeeb6ce0eba79b53a66af/weakmap.js" $mod_path"/weakmap/index.js"
 
-if [[ $1 == "dev" ]]; then
+if Joiner:with_dev ; then
     Joiner:add_repo "https://github.com/mochajs/mocha" "mocha" "1.20.1" $mod_path
     
     Joiner:add_file "http://sinonjs.org/releases/sinon-1.10.3.js" $mod_path"/sinon/index.js"
